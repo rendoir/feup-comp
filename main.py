e2dc67238ea569bd60be7cd8d304e92111373a3e
Almeida-Oco <intjalmeida@gmail.com>
2018-02-24 04:04:14 +0000

    Commented main

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from antlr4 import *
 from ParserLexer import ParserLexer
 from ParserParser import ParserParser
 
+# ParserParser class will have a method for each 'rule' in the parser.
+# So since the parser has rules such as 'vector', 'expression' and 'pyClass'
+# It generates a method to parse each one of these
 def main(argv):
-    input = FileStream(argv[1])
-    lexer = ParserLexer(input)
-    stream = CommonTokenStream(lexer)
-    parser = ParserParser(stream)
-    print(parser.pyClass().toStringTree(recog=parser))
+    input = FileStream(argv[1])                         # Open the specified file for input
+    lexer = ParserLexer(input)                          # Create a lexer parser
+    stream = CommonTokenStream(lexer)                   # No clue what this is
+    parser = ParserParser(stream)                       # The actual parser
+    print(parser.pyClass().toStringTree(recog=parser))  # Prints the matches, if there are multiple matches, need to call parse.pyClass() multiple times
 
 
 if __name__ == '__main__':
