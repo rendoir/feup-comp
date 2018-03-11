26cfcb388951d84fa09e6569cd887cc346d244fd
Almeida-Oco <intjalmeida@gmail.com>
2018-03-11 02:09:54 +0000

    Added support for multicharacters Added coloring for the word underline

--- yalErrorListener.py
+++ yalErrorListener.py
@@ -1,10 +1,20 @@
 from antlr4.error.ErrorListener import *
 from pprint import pprint
 
+RED = "\033[1;31m"
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD    = "\033[;1m"
+REVERSE = "\033[;7m"
+
 class yalErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         text_input = recognizer.getInputStream().tokenSource.inputStream.strdata
         print("\n" + text_input.split('\n')[line-1])
         for i in range(column):
             print(" ", end='')
-        print("^")
+        for i in range(len(offendingSymbol.text)):
+            print(RED + "^", end='')
+        print(RESET + "\n")
