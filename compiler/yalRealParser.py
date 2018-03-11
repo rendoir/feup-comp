72b719bd695a7a5cf40b2efd29144c8cce213c2f
Almeida-Oco <intjalmeida@gmail.com>
2018-03-11 18:13:36 +0000

    Added a simple function argument extracter Parser does not continue if it has syntax errors

---
+++ compiler/yalRealParser.py
@@ -0,0 +1,23 @@
+from yalParser import yalParser
+from antlr4.Token import Token
+
+prune = ['{', '}', ';', ',']
+
+class yalRealParser(yalParser):
+    def consume(self):
+        o = self.getCurrentToken()
+        if o.type != Token.EOF:
+            self.getInputStream().consume()
+        hasListener = self._parseListeners is not None and len(self._parseListeners)>0
+        if self.buildParseTrees or hasListener:
+            if self._errHandler.inErrorRecoveryMode(self):
+                node = self._ctx.addErrorNode(o)
+            elif o.text not in prune:
+                node = self._ctx.addTokenNode(o)
+            if hasListener:
+                for listener in self._parseListeners:
+                    if isinstance(node, ErrorNode):
+                        listener.visitErrorNode(node)
+                    elif isinstance(node, TerminalNode):
+                        listener.visitTerminal(node)
+        return o
--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import sys
 from antlr4 import *
-from antlr4.error.ErrorStrategy import *
 from yalLexer import yalLexer
-from yalParser import yalParser
-from yalListener import yalListener
-from yalErrorStrategy import yalErrorStrategy
-from yalErrorListener import yalErrorListener
+from compiler.yalErrorStrategy import *
+from compiler.yalErrorListener import *
+from compiler.yalRealParser import *
 
-
-class Listener(yalListener):
-    def enterModule(self, ctx:yalParser.ModuleContext):
-        print("Module = " + ctx.getText())
-
-    def enterArray_element(self, ctx:yalParser.Array_elementContext):
-        print("Elem = " + ctx.getText())
-
-    def enterScalar_element(self, ctx:yalParser.Scalar_elementContext):
-        print("Scalar = " + ctx.getText())
+from pprint import pprint
 
 # ParserParser class will have a method for each 'rule' in the parser.
 # So since the parser has rules such as 'vector', 'expression' and 'pyClass'
@@ -25,20 +14,48 @@ def main(argv):
     input = FileStream(argv[1])                         # Open the specified file for input
     lexer = yalLexer(input)                             # Create a lexer parser
     stream = CommonTokenStream(lexer)                   # No clue what this is
-    parser = yalParser(stream)                          # The actual parser
+    parser = yalRealParser(stream)                          # The actual parser
     parser.addErrorListener(yalErrorListener())
-    # parser._errHandler = yalErrorStrategy()
+    parser._errHandler = yalErrorStrategy()
     tree = parser.module()                              # Start the parser (error handling should be before this)
+
+    if parser.getNumberOfSyntaxErrors() > 0:
+        print(" -> " + RED + str(parser.getNumberOfSyntaxErrors()) + RESET + " Syntax errors detected!")
+        return
+
+    children = tree.function(0).getChildren(lambda x: not isinstance(x, TerminalNode))
+    tree.function(0).getTokens(0)
+    print(getFuncArgs(tree.function(0)))
+    #tree.function(0) gets the first function
     # print(tree.toStringTree(recog=parser))
 
 
+#* @description Extracts the function arguments
+#* @arg `root` - The root node of the function in the syntax tree
+#* @return A dictionary where keys are the argument name
+#*       value is a boolean of whether the argument is an array or not
+#*
+#* @detail It starts by getting the children of the function which are not terminal symbols so it extracts everything but the arguments.
+#* After that extractArg is called for every children of the child obtained (the child is the 'var_list' antlr4 rule)
+def getFuncArgs(root):
+    func_children = root.getChildren(lambda x: not isinstance(x, TerminalNode))
+    vars = next(func_children).getChildren()
+    arguments = {}
 
+    for var in vars:
+        arg = extractArg(var)
+        arguments[arg["name"]] = arg["array"]
 
-    # walker = ParseTreeWalker()
-    # listener = Listener()
-    # walker.walk(listener, tree)                         # Walks the tree calling the listener when it enters a rule
-    # print(tree.toStringTree(recog=parser))   # Prints the matches
+    return arguments
 
+def extractArg(argument):
+    if argument.getChildCount() == 1:
+        return {"name": next(argument.getChildren()).getText(), "array": False}
+    elif argument.getChildCount() == 3:
+        return {"name": next(argument.getChildren()).getText(), "array": True}
+    else:
+        print("Error! argument has " + argument.getChildCount() + " args")
+        return None
 
 if __name__ == '__main__':
     main(sys.argv)
