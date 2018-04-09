33c06b885c525b7083ef2483192f8439174981e8
Almeida-Oco <intjalmeida@gmail.com>
2018-04-09 11:23:35 +0100

    ss

--- compiler/HIR/Function.py
+++ compiler/HIR/Function.py
@@ -1,17 +1,42 @@
 from  antlr_yal import *
+from compiler.HIR.ArrayVariable import ArrayVariable
+from compiler.HIR.NumberVariable import NumberVariable
 
 class Function:
     def __init__(self, ret_var: str, args: yalParser.Arg_listContext, stmts: yalParser.Stmt_listContext):
         self.ret_var = ret_var;
-        self.arguments = list ();
+        self.arguments = {};
         self.stmts = list();
         if args is not None:
             self.__addArgs(args)
+        for key, item in self.arguments.items():
+            print("args = " + key);
+
         if stmts is not None:
-            self.__addStmts(stmts)
+            self.__addStmts(stmts.getChildren())
 
     def __addArgs(self, args: yalParser.Arg_listContext):
-        print("Adding #" + str(args.getChildCount()) + " args!");
+        children = args.getChildren()
+        for child in children:
+            var = child.split('[]')
+            if len(var) is 2:
+                self.arguments[var[0]] = ArrayVariable(None, 0, 0)
+            else:
+                self.arguments[var[0]] = NumberVariable(None, 0, 0)
 
-    def __addStmts(self, stmts: yalParser.Stmt_listContext):
-        print("Adding #" + str(stmts.getChildCount()) + " stmts!");
+                #TODO add hinting
+    def __addStmts(self, stmts):
+        for stmt in stmts:
+            for stmtt in stmt.getChildren():
+                print(" STMTM " + str(stmtt));
+
+            print(stmt.__class__.__name__)
+            if isinstance(stmt, yalParser.While_yalContext):
+                print("WHILE");
+            elif isinstance(stmt, yalParser.If_yalContext):
+                print("IF");
+            elif isinstance(stmt, yalParser.AssignContext):
+                print("ASSIGN");
+            elif isinstance(stmt, yalParser.CallContext):
+                print("SD = " + stmt.getChildren[0])
+                breadcrumbs = stmt.split('.')
