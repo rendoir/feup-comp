9dca6b752a991af12e6a63876a4340c7ea0584e7
rendoir <up201503822@fe.up.pt>
2018-04-30 12:27:10 +0100

    Generating code: module vars

--- compiler/LIR/generateCode.py
+++ compiler/LIR/generateCode.py
@@ -1,9 +1,26 @@
 from compiler.HIR.CodeScope import *
 
+NL = '\n'
+
 def generateCode(module, in_file):
     print('--- BEGIN GENERATING CODE ---')
-    out_file = in_file.split('.')[0] + '._j' #TODO REMOVE UNDERSCORE
+    out_file = in_file.split('.')[0] + '.tmp' #TODO CHANGE .tmp TO .j
     with open(out_file, 'w') as out:
-        
+        #TODO REMOVE PRINTS
+        #print(module.vars)
+        #print(module.code)
+
+        #Module
+        out.write(".class public " + module.name + NL)
+        out.write(".super java/lang/Object" + NL + NL)
+
+        #Module Vars
+        for var in module.vars:
+            out.write(".field static " + module.vars[var].name + " ")
+            if(module.vars[var].type == "ARR"):
+                out.write("[")
+            out.write("I")
+            out.write(NL)
+
         out.close()
     print('--- END GENERATING CODE ---')
