a2ed4d0c1ed116ade6164771bc2629a42058e4f1
rendoir <up201503822@fe.up.pt>
2018-05-02 20:15:14 +0100

    Feedback

--- compiler/LIR/generateCode.py
+++ compiler/LIR/generateCode.py
@@ -177,7 +177,7 @@ def __writeTerm(term, out, mod_name):
     elif isinstance(term.value, ScalarAccess):
         __writeScalarAccess(term.value, out)
     else:
-        out.write('ldc ' + str(self.value) + NL)
+        out.write('ldc ' + str(term.value) + NL)
 
 def __writeCall(call, out, mod_name):
     processArgsLoading(call, out)
